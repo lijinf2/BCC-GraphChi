45874009d25840177f87b253b9b8ee904d94e7c3
lijinf2 <lijinf8@gmail.com>
2014-04-30 10:22:28 +0800

    first commit

---
+++ 2drun.sh
@@ -0,0 +1,5 @@
+bin/my_app/twodpartition file ../data/blodata/USA-road-d.NY.gr membudget_mb 10 coordinate ../data/blodata/USA-road-d.NY.co sampleRate 0.01 niters 1000 scheduler 1  xnum  20 ynum 20
+#bin/my_app/twodpartition file ../data/blodata/FLA/USA-road-d.FLA.gr membudget_mb 10 coordinate ../data/blodata/FLA/USA-road-d.FLA.co sampleRate 0.01 niters 1000 scheduler 1  xnum 20 ynum 20
+#bin/my_app/twodpartition file ../data/blodata/W/USA-road-d.W.gr membudget_mb 100 coordinate ../data/blodata/W/USA-road-d.W.co sampleRate 0.01 niters 1000 scheduler 1  xnum 300 ynum 300
+#bin/my_app/twodpartition file ../data/blodata/CTR/USA-road-d.CTR.gr membudget_mb 100 coordinate ../data/blodata/CTR/USA-road-d.CTR.co sampleRate 0.01 niters 1000 scheduler 1  xnum 600 ynum 600
+
